3e75d19b6ee5f920999209ea971c368e6787bf8d
Umaimakhan12 <164545606+Umaimakhan12@users.noreply.github.com>
2024-03-25 09:46:48 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// printing a wellcome Message
+console.log("\n\twellcome To \ 'umaimakhan/' - CLI simple calculator\n");
+//Asking questions form users through Inquirer
+let answers = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "select one operator to perfoms operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subraction", "Multiplication", "Division"],
+    },
+]);
+//Conditional statements If-Else
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Subraction") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("Invalid Input");
+}
